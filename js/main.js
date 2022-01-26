63fee30d97e34a4768f11f7d281db49c35909fd9
Karen <40191282+jrkaren9@users.noreply.github.com>
2022-01-25 23:57:05 -0300

    Desafío con funciones

--- js/main.js
+++ js/main.js
@@ -1,18 +1,52 @@
+const products = {
+    total: 2,
+    items: [{
+        description: "shirt",
+        cost: 85
+    }, {
+        description: "jersey",
+        cost: "120"
+    }]
+}
+
+function findCost(item){
+    for (let i = 0; i < products.items.length; i++) {
+        if(products.items[i].description == item)
+            return products.items[i].cost;
+    } 
+}
+
+function addItems(amount, item, total) {
+    
+    let cost = findCost(item);
+
+    if(cost != null) {
+        for (let i = 0; i < amount; i++) {
+            if (i > 2) {
+                total += cost*0.9;5
+            }
+            else {
+                total += cost;
+            }
+        }   
+    
+        console.log(total);
+        return total;
+    }
+
+    else 
+        return 0;
+}
+
 let exit = false;
 
 do {
-    let total = 0, cost = 85;
-    let amount = prompt("How many items do you want to add? After three, each new one has a discount of 10%");
-        amount = parseInt(amount);
+    let total = 0;
+    let item = 'shirt';
+    
+    let amount = parseInt(prompt("How many " + item + " do you want to add? After three, each new one has a discount of 10%"));
 
-    for (let i = 0; i < amount; i++) {
-        if (i > 2) {
-            total += cost*0.9;5
-        }
-        else {
-            total += cost;
-        }
-    }
+    total = addItems(amount, item, total);
 
     exit = confirm("The total is $" + total.toFixed(2) + ". Do you want to pay?");
 
